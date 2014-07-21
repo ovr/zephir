f820c656a9146f4c28824439bcdded8281d37e07
Дмитрий Пацура <zaets28rus@gmail.com>
2014-07-21 12:00:07 +0400

    Write ClassDefinition

--- Library/Definitions/ClassDefinition/MethodDefintion.php
+++ Library/Definitions/ClassDefinition/MethodDefintion.php
@@ -17,61 +17,9 @@
  +--------------------------------------------------------------------------+
 */
 
-namespace Zephir\Definitions;
+namespace Zephir\Definitions\ClassDefinition;
 
-use Zephir\Definitions\ClassDefinition\MethodDefinition;
-
-class ClassDefinition
+class MethodDefinition
 {
-    /**
-     * @var string
-     */
-    const CLASS_TYPE = 'class';
 
-    /**
-     * @var string
-     */
-    const INTERFACE_TYPE = 'interface';
-
-    /**
-     * @var string|null
-     */
-    private $docBlock;
-
-    /**
-     * @var MethodDefinition[]
-     */
-    private $methods = [];
-
-    /**
-     * Class name
-     *
-     * @var string
-     */
-    private $name;
-
-    private $type = self::CLASS_TYPE;
-
-    /**
-     * @param string $name ClassName
-     * @param string|null $docBlock phpdoc comment for class
-     */
-    public function __construct($name, $docBlock = null)
-    {
-        $this->name = $name;
-        $this->docBlock = $docBlock;
-    }
-
-    /**
-     * @return array
-     */
-    public function getMethods()
-    {
-        return $this->methods;
-    }
-
-    public function addMethod(MethodDefinition $methods)
-    {
-        $this->methods = $methods;
-    }
 }
